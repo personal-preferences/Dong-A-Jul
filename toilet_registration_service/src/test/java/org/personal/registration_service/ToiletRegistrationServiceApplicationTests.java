3fc1b058b864db6eae90c44520b10678335a9da9
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-01 14:05:42 +0900

    init: 환경설정
    프로젝트 초기 설정

---
+++ toilet_registration_service/src/test/java/org/personal/registration_service/ToiletRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiletRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
