10ea7bef1a92063fa77aa3cd17f8acfd552161b6
daersh <daersh@naver.com>
2024-08-01 14:57:51 +0900

    feat(DateParsing): 날짜 포멧 변경 기능 추가
    String to LocalDateTime || LocalDateTimeToString
    두가지로 변환 가능하도록 기능 추가했습니다.

---
+++ user_service/src/main/java/org/personal/user_service/config/DateParsing.java
@@ -0,0 +1,18 @@
+package org.personal.user_service.config;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class DateParsing {
+    public static LocalDateTime strToLdt(String str){
+        if (!Objects.equals(str, ""))
+            return LocalDateTime.parse(str,DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return null;
+    }
+    public static String LdtToStr(LocalDateTime localDateTime){
+        if (localDateTime!=null)
+            return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return null;
+    }
+}
