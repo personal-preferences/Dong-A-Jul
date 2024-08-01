d64c7087ac1b1b43f2c9163e8985e6969b8d1f8e
CJC0512 <ehdchs2006@gmail.com>
2024-08-01 15:32:50 +0900

    feat(Review): QueryDSL 적용
    QueryDSL 적용을 위한 gradle 및 JpaConfig 추가

--- review_service/src/main/java/org/personal/review_service/repository/ReviewRepository.java
+++ review_service/src/main/java/org/personal/review_service/repository/ReviewRepository.java
@@ -1,9 +1,34 @@
-package org.personal.review_service.domain.repository;
+package org.personal.review_service.repository;
 
-import org.personal.review_service.domain.aggregate.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.personal.review_service.domain.QReview;
+import org.personal.review_service.domain.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
+import java.util.List;
 
+@Repository
+public class ReviewRepository {
 
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
+    public List<Review> findReviewsByUserId(Long userId) {
+        QReview review = QReview.review;
+
+        return jpaQueryFactory
+                .selectFrom(review)
+                .where(review.userId.eq(userId))
+                .fetch();
+    }
+
+    public List<Review> findReviewsByLocationId(Long locationId) {
+        QReview review = QReview.review;
+
+        return jpaQueryFactory
+                .selectFrom(review)
+                .where(review.locationId.eq(locationId))
+                .fetch();
+    }
 }
