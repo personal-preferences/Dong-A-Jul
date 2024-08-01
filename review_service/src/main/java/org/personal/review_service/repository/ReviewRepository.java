4b60962197fc9507cfb3330c404572c53c65a7c5
CJC0512 <ehdchs2006@gmail.com>
2024-08-01 16:31:16 +0900

    fix(Review): Repository extends 오류 수정
    ReviewRepository의 extends
    JpaRepository<Review, Long> -> ReviewJpaRepository
    로 수정

--- review_service/src/main/java/org/personal/review_service/repository/ReviewRepository.java
+++ review_service/src/main/java/org/personal/review_service/repository/ReviewRepository.java
@@ -1,9 +1,7 @@
 package org.personal.review_service.repository;
 
-import org.personal.review_service.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom{
+public interface ReviewRepository extends ReviewJpaRepository, ReviewRepositoryCustom{
     // JPA 쿼리 메서드와 커스텀 메서드를 모두 사용 가능
 
 }
