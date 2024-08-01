62d768ac98258757c94499a915aeb53843d1d758
ms1011 <wkdalstjr94@gmail.com>
2024-08-01 15:54:12 +0900

    test(location_service): 화장실 등록 Controller 성공 테스트 추가

--- location_service/src/main/java/org/personal/locations_service/controller/LocationController.java
+++ location_service/src/main/java/org/personal/locations_service/controller/LocationController.java
@@ -2,12 +2,10 @@ package org.personal.locations_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.personal.locations_service.request.LocationCreate;
 import org.personal.locations_service.response.LocationResponse;
 import org.personal.locations_service.service.LocationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -16,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
 
     private final LocationService locationService;
+
+    @PostMapping
+    public void add(@RequestBody LocationCreate locationCreate) {
+        locationService.add(locationCreate);
+    }
 
     @GetMapping("/{toiletName}")
     public LocationResponse get(@PathVariable String toiletName){
