c105e82ec57b17cbf6e1120f737fd1ebc898e459
Sosohy <sh07202@naver.com>
2024-08-01 14:21:56 +0900

    init(info-service) : 화장실 정보 서비스 프로젝트 생성

---
+++ toilet_info_service/src/main/java/org/personal/toilet_info_service/ToiletInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package org.personal.toilet_info_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiletInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToiletInfoServiceApplication.class, args);
+	}
+
+}
