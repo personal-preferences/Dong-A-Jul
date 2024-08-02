b3d1950894f6ecd0d0d61c2c55eeac02fd7e64c4
CJC0512 <ehdchs2006@gmail.com>
2024-08-02 11:27:18 +0900

    fix(Review): Repository 구조 수정
    불필요한 코드 제거 및 JpaRepository 직접 extends

--- review_service/src/main/java/org/personal/review_service/repository/ReviewJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.personal.review_service.repository;
-
-import org.personal.review_service.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewJpaRepository extends JpaRepository<Review, Long> {
-    // JPA 쿼리 메서드 정의
-}
--- review_service/src/main/java/org/personal/review_service/repository/ReviewRepository.java
+++ review_service/src/main/java/org/personal/review_service/repository/ReviewRepository.java
@@ -1,7 +1,10 @@
 package org.personal.review_service.repository;
 
 
-public interface ReviewRepository extends ReviewJpaRepository, ReviewRepositoryCustom{
+import org.personal.review_service.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom{
     // JPA 쿼리 메서드와 커스텀 메서드를 모두 사용 가능
 
 }
