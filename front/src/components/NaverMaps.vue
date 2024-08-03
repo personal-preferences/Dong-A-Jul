cf72aecc1e189a809052c99e306c0003d9a9bb42
ms1011 <wkdalstjr94@gmail.com>
2024-08-04 01:13:05 +0900

    feat(location): 네이버 지도 api 통신 확인
    api client-ID는 .env파일로 따로 관리하고 있습니다

---
+++ front/src/components/NaverMaps.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="map" ref="mapRef" style="width:400px;height:400px;"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const mapRef = ref(null);
+
+onMounted(async () => {
+  try {
+    await loadNaverMapsScript();
+
+    if (window.naver && window.naver.maps) {
+      const mapOptions = {
+        center: new window.naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10
+      };
+
+      const res = new window.naver.maps.Map(mapRef.value, mapOptions);
+    } else {
+      console.error('Naver Maps API is not loaded');
+    }
+  } catch (error) {
+    console.error('Error initializing map:', error);
+  }
+});
+
+function loadNaverMapsScript() {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${import.meta.env.VITE_NAVER_MAPS_CLIENT_ID}`;
+    script.async = true;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+</script>
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NaverMaps from "@/components/NaverMaps.vue";
+</script>
+
+<template>
+<NaverMaps />
+</template>
+
+<style scoped>
+
+</style>
