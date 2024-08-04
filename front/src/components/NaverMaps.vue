44009e44d4712d1daa66a633ed913d557685baa1
ms1011 <wkdalstjr94@gmail.com>
2024-08-04 13:44:51 +0900

    feat(location): 보이는 지도 영역의 마커만 표시하기
    화장실 표시는 🚽로 표기

--- front/src/components/NaverMaps.vue
+++ front/src/components/NaverMaps.vue
@@ -6,6 +6,40 @@
 import { onMounted, ref } from 'vue';
 
 const mapRef = ref(null);
+const MARKER_SPRITE_X_OFFSET = 29;
+const MARKER_SPRITE_Y_OFFSET = 50;
+const MARKER_SPRITE_POSITION = {
+  "A0": [0, 0],
+  "B0": [MARKER_SPRITE_X_OFFSET, 0],
+  "C0": [MARKER_SPRITE_X_OFFSET*2, 0],
+  "D0": [MARKER_SPRITE_X_OFFSET*3, 0],
+  "E0": [MARKER_SPRITE_X_OFFSET*4, 0],
+  "F0": [MARKER_SPRITE_X_OFFSET*5, 0],
+  "G0": [MARKER_SPRITE_X_OFFSET*6, 0],
+  "H0": [MARKER_SPRITE_X_OFFSET*7, 0],
+  "I0": [MARKER_SPRITE_X_OFFSET*8, 0],
+
+  "A1": [0, MARKER_SPRITE_Y_OFFSET],
+  "B1": [MARKER_SPRITE_X_OFFSET, MARKER_SPRITE_Y_OFFSET],
+  "C1": [MARKER_SPRITE_X_OFFSET*2, MARKER_SPRITE_Y_OFFSET],
+  "D1": [MARKER_SPRITE_X_OFFSET*3, MARKER_SPRITE_Y_OFFSET],
+  "E1": [MARKER_SPRITE_X_OFFSET*4, MARKER_SPRITE_Y_OFFSET],
+  "F1": [MARKER_SPRITE_X_OFFSET*5, MARKER_SPRITE_Y_OFFSET],
+  "G1": [MARKER_SPRITE_X_OFFSET*6, MARKER_SPRITE_Y_OFFSET],
+  "H1": [MARKER_SPRITE_X_OFFSET*7, MARKER_SPRITE_Y_OFFSET],
+  "I1": [MARKER_SPRITE_X_OFFSET*8, MARKER_SPRITE_Y_OFFSET],
+
+  "A2": [0, MARKER_SPRITE_Y_OFFSET*2],
+  "B2": [MARKER_SPRITE_X_OFFSET, MARKER_SPRITE_Y_OFFSET*2],
+  "C2": [MARKER_SPRITE_X_OFFSET*2, MARKER_SPRITE_Y_OFFSET*2],
+  "D2": [MARKER_SPRITE_X_OFFSET*3, MARKER_SPRITE_Y_OFFSET*2],
+  "E2": [MARKER_SPRITE_X_OFFSET*4, MARKER_SPRITE_Y_OFFSET*2],
+  "F2": [MARKER_SPRITE_X_OFFSET*5, MARKER_SPRITE_Y_OFFSET*2],
+  "G2": [MARKER_SPRITE_X_OFFSET*6, MARKER_SPRITE_Y_OFFSET*2],
+  "H2": [MARKER_SPRITE_X_OFFSET*7, MARKER_SPRITE_Y_OFFSET*2],
+  "I2": [MARKER_SPRITE_X_OFFSET*8, MARKER_SPRITE_Y_OFFSET*2]
+};
+
 
 onMounted(async () => {
   try {
@@ -18,11 +52,40 @@ onMounted(async () => {
       };
 
       const map = new window.naver.maps.Map(mapRef.value, mapOptions);
+      const bounds = map.getBounds();
+      const southWest = bounds.getSW();
+      const northEast = bounds.getNE();
+      const lngSpan = northEast.lng() - southWest.lng();
+      const latSpan = northEast.lat() - southWest.lat();
 
-      const marker = new window.naver.maps.Marker({
-        position: new window.naver.maps.LatLng(37.3595704, 127.105399),
-        map: map
+      const markers = [];
+
+      for (const key in MARKER_SPRITE_POSITION) {
+        const position = new window.naver.maps.LatLng(
+            southWest.lat() + latSpan * Math.random(),
+            southWest.lng() + lngSpan * Math.random()
+        );
+
+        const marker = new window.naver.maps.Marker({
+          map: map,
+          position: position,
+          title: key,
+          icon: {
+            content: '<p>🚽</p>',
+            size: new window.naver.maps.Size(24, 37),
+            anchor: new window.naver.maps.Point(12, 37),
+            origin: new window.naver.maps.Point(MARKER_SPRITE_POSITION[key][0], MARKER_SPRITE_POSITION[key][1])
+          },
+          zIndex: 100
+        });
+
+        markers.push(marker);
+      }
+
+      window.naver.maps.Event.addListener(map, 'idle', () => {
+        updateMarkers(map, markers);
       });
+
     } else {
       console.error('Naver Maps API is not loaded');
     }
@@ -40,5 +103,27 @@ function loadNaverMapsScript() {
     script.onerror = reject;
     document.head.appendChild(script);
   });
+}
+
+function updateMarkers(map, markers) {
+  const mapBounds = map.getBounds();
+  markers.forEach(marker => {
+    const position = marker.getPosition();
+    if (mapBounds.hasLatLng(position)) {
+      showMarker(map, marker);
+    } else {
+      hideMarker(map, marker);
+    }
+  });
+}
+
+function showMarker(map, marker) {
+  if (marker.getMap()) return;
+  marker.setMap(map);
+}
+
+function hideMarker(map, marker) {
+  if (!marker.getMap()) return;
+  marker.setMap(null);
 }
 </script>
