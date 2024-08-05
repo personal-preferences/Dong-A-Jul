42acc345618682cfa5cad8024a1d6a0bb5be4a9b
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-05 21:50:03 +0900

    Feat(regist): Repository 테스트
    SpringBootTest -> DataJpaTest 어노테이션 바꿈

--- toilet_registration_service/src/test/java/org/personal/registration_service/repository/ToiletRegistRepositoryTests.java
+++ toilet_registration_service/src/test/java/org/personal/registration_service/repository/ToiletRegistRepositoryTests.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+@DataJpaTest
 class ToiletRegistRepositoryTests {
 
 	@Autowired
