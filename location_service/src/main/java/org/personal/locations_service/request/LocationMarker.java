83fd99a57ce00536836e67a5320add1ca73fca0a
ms1011 <wkdalstjr94@gmail.com>
2024-08-05 18:09:42 +0900

    fix(location): `@NotNull` 검증을 하기 위하여 float->Float으로 변경
    float은 null을 담을 수 없으므로 0.0f가 들어가 `@NotNull`이 원하는 방식으로 작동하지 않아서 변경

--- location_service/src/main/java/org/personal/locations_service/request/LocationMarker.java
+++ location_service/src/main/java/org/personal/locations_service/request/LocationMarker.java
@@ -1,13 +1,30 @@
 package org.personal.locations_service.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 public record LocationMarker(
-        @NotBlank(message = "북동쪽 위도를 입력하세요.") float northEastLatitude,
-        @NotBlank(message = "북동쪽 경도를 입력하세요.") float northEastLongitude,
-        @NotBlank(message = "서남쪽 위도를 입력하세요.") float southWestLatitude,
-        @NotBlank(message = "서남쪽 경도를 입력하세요.") float southWestLongitude
+        @NotNull(message = "북동쪽 위도를 입력하세요.")
+        @Min(value = -90, message = "위도는 -90보다 크거나 같아야 합니다.")
+        @Max(value = 90, message = "위도는 90보다 작거나 같아야 합니다.")
+        Float northEastLatitude,
+
+        @NotNull(message = "북동쪽 경도를 입력하세요.")
+        @Min(value = -180, message = "경도는 -180보다 크거나 같아야 합니다.")
+        @Max(value = 180, message = "경도는 180보다 작거나 같아야 합니다.")
+        Float northEastLongitude,
+
+        @NotNull(message = "서남쪽 위도를 입력하세요.")
+        @Min(value = -90, message = "위도는 -90보다 크거나 같아야 합니다.")
+        @Max(value = 90, message = "위도는 90보다 작거나 같아야 합니다.")
+        Float southWestLatitude,
+
+        @NotNull(message = "서남쪽 경도를 입력하세요.")
+        @Min(value = -180, message = "경도는 -180보다 크거나 같아야 합니다.")
+        @Max(value = 180, message = "경도는 180보다 작거나 같아야 합니다.")
+        Float southWestLongitude
 ) {
     @Builder
     public LocationMarker {
