560de5da6642c45b0df93ace8404f129338bf17c
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-06 14:02:22 +0900

    Feat(regist): Repository test
    이미 등록신청된 화장실인지 테스트 구현, 테스트 성공

--- toilet_registration_service/src/test/java/org/personal/registration_service/repository/ToiletRegistRepositoryTests.java
+++ toilet_registration_service/src/test/java/org/personal/registration_service/repository/ToiletRegistRepositoryTests.java
@@ -45,4 +45,33 @@ class ToiletRegistRepositoryTests {
 		assertThat(result.getToiletRegistLongitude()).isEqualTo(toiletRegist.getToiletRegistLongitude());
 	}
 
+	@Test
+	public void 등록신청된화장실인지테스트(){
+		// given
+		final ToiletRegist toiletRegist = ToiletRegist.builder()
+			.toiletRegistImg("sample_image.png")
+			.toiletRegistToiletName("Sample Toilet")
+			.toiletRegistRoadNameAddress("123 Sample Road")
+			.toiletRegistNumberAddress("456")
+			.toiletRegistLatitude(37.123456)
+			.toiletRegistLongitude(127.123456)
+			.build();
+
+		// when
+		toiletRegistRepository.save(toiletRegist);
+		final ToiletRegist findResult = toiletRegistRepository
+			.findByToiletRegistLatitudeAndToiletRegistLongitude(37.123456, 127.123456);
+
+		// then
+		assertThat(findResult).isNotNull();
+		assertThat(findResult.getToiletRegistId()).isNotNull();
+		assertThat(findResult.getToiletRegistDate()).isEqualTo(toiletRegist.getToiletRegistDate());
+		assertThat(findResult.getToiletRegistImg()).isEqualTo(toiletRegist.getToiletRegistImg());
+		assertThat(findResult.getToiletRegistIsApproved()).isEqualTo(toiletRegist.getToiletRegistIsApproved());
+		assertThat(findResult.getToiletRegistToiletName()).isEqualTo(toiletRegist.getToiletRegistToiletName());
+		assertThat(findResult.getToiletRegistRoadNameAddress()).isEqualTo(toiletRegist.getToiletRegistRoadNameAddress());
+		assertThat(findResult.getToiletRegistNumberAddress()).isEqualTo(toiletRegist.getToiletRegistNumberAddress());
+		assertThat(findResult.getToiletRegistLatitude()).isEqualTo(toiletRegist.getToiletRegistLatitude());
+		assertThat(findResult.getToiletRegistLongitude()).isEqualTo(toiletRegist.getToiletRegistLongitude());
+	}
 }
