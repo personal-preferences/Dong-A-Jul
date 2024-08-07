8a99f1a9d5dde88babcd186a912283b6a584eef6
Sosohy <sh07202@naver.com>
2024-08-07 19:23:39 +0900

    chore(info_service) : queryDSL 세팅

---
+++ info_service/src/main/java/org/personal/info_service/config/QueryDslConfig.java
@@ -0,0 +1,17 @@
+package org.personal.info_service.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() { return new JPAQueryFactory(em); }
+}
