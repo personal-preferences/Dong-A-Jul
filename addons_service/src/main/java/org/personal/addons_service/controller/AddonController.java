f37eeb4d687dd26945462492e90de653084657e0
orlzlL <yerincho06@gmail.com>
2024-08-10 13:15:27 +0900

    feat(addon) : 애드온 등록 기능 컨트롤러 추가

--- addons_service/src/main/java/org/personal/addons_service/controller/AddonController.java
+++ addons_service/src/main/java/org/personal/addons_service/controller/AddonController.java
@@ -1,9 +1,40 @@
 package org.personal.addons_service.controller;
 
+import org.personal.addons_service.exception.AddonErrorResult;
+import org.personal.addons_service.exception.AddonException;
+import org.personal.addons_service.request.CreateAddonRequest;
+import org.personal.addons_service.service.AddonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/addons")
 public class AddonController {
+
+	private final AddonService addonService;
+
+	@Autowired
+	public AddonController(AddonService addonService) {
+		this.addonService = addonService;
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> createAddon(@RequestBody @Valid CreateAddonRequest request) {
+		try {
+			addonService.createAddon(request);
+			return ResponseEntity.status(HttpStatus.CREATED).build();
+		} catch (AddonException e) {
+			if (e.getErrorResult() == AddonErrorResult.DUPLICATED_ADDON_CREATE) {
+				return ResponseEntity.status(HttpStatus.CONFLICT).build();
+			}
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
 }
--- addons_service/src/main/java/org/personal/addons_service/request/CreateAddonRequest.java
+++ addons_service/src/main/java/org/personal/addons_service/request/CreateAddonRequest.java
@@ -1,12 +1,13 @@
 package org.personal.addons_service.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateAddonRequest (String memoContent,
 								  boolean isBookmarked,
 								  @NotBlank String userEmail,
-								  @NotBlank Long toiletLocationId){
+								  @NotNull Long toiletLocationId){
 
 	@Builder
 	public CreateAddonRequest(String memoContent, boolean isBookmarked, String userEmail, Long toiletLocationId) {
