24cc8366b7d7ee0e02f9821097e2467a4c10fd81
ms1011 <wkdalstjr94@gmail.com>
2024-08-10 19:43:58 +0900

    feat(location): 반환형이 있는 요청에 Response Entity 사용하여 반환

--- location_service/src/main/java/org/personal/locations_service/controller/LocationController.java
+++ location_service/src/main/java/org/personal/locations_service/controller/LocationController.java
@@ -10,6 +10,8 @@ import org.personal.locations_service.response.LocationResponse;
 import org.personal.locations_service.service.LocationService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,13 +30,21 @@ public class LocationController {
     }
 
     @GetMapping("/{toiletName}")
-    public LocationResponse get(@PathVariable String toiletName){
-        return locationService.get(toiletName);
+    public ResponseEntity<LocationResponse> get(@PathVariable String toiletName){
+        LocationResponse response = locationService.get(toiletName);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 
     @GetMapping
-    public List<LocationResponse> getList(@RequestBody @Valid LocationMarker request) {
-        return locationService.getList(request);
+    public ResponseEntity<List<LocationResponse>> getList(@RequestBody @Valid LocationMarker request) {
+        List<LocationResponse> response = locationService.getList(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 
     @PatchMapping("/{locationId}")
