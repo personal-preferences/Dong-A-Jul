253829869da91b154f76d9f3c829cdf1d011dee5
ms1011 <wkdalstjr94@gmail.com>
2024-08-11 19:20:51 +0900

    refactor(location): 오류 메시지에 위치 추가

--- location_service/src/main/java/org/personal/locations_service/request/LocationMarker.java
+++ location_service/src/main/java/org/personal/locations_service/request/LocationMarker.java
@@ -7,23 +7,23 @@ import lombok.Builder;
 
 public record LocationMarker(
         @NotNull(message = "북동쪽 위도를 입력하세요.")
-        @Min(value = -90, message = "위도는 -90보다 크거나 같아야 합니다.")
-        @Max(value = 90, message = "위도는 90보다 작거나 같아야 합니다.")
+        @Min(value = -90, message = "북동쪽 위도는 -90보다 크거나 같아야 합니다.")
+        @Max(value = 90, message = "북동쪽 위도는 90보다 작거나 같아야 합니다.")
         Float northEastLatitude,
 
         @NotNull(message = "북동쪽 경도를 입력하세요.")
-        @Min(value = -180, message = "경도는 -180보다 크거나 같아야 합니다.")
-        @Max(value = 180, message = "경도는 180보다 작거나 같아야 합니다.")
+        @Min(value = -180, message = "북동쪽 경도는 -180보다 크거나 같아야 합니다.")
+        @Max(value = 180, message = "북동쪽 경도는 180보다 작거나 같아야 합니다.")
         Float northEastLongitude,
 
         @NotNull(message = "서남쪽 위도를 입력하세요.")
-        @Min(value = -90, message = "위도는 -90보다 크거나 같아야 합니다.")
-        @Max(value = 90, message = "위도는 90보다 작거나 같아야 합니다.")
+        @Min(value = -90, message = "서남쪽 위도는 -90보다 크거나 같아야 합니다.")
+        @Max(value = 90, message = "서남쪽 위도는 90보다 작거나 같아야 합니다.")
         Float southWestLatitude,
 
         @NotNull(message = "서남쪽 경도를 입력하세요.")
-        @Min(value = -180, message = "경도는 -180보다 크거나 같아야 합니다.")
-        @Max(value = 180, message = "경도는 180보다 작거나 같아야 합니다.")
+        @Min(value = -180, message = "서남쪽 경도는 -180보다 크거나 같아야 합니다.")
+        @Max(value = 180, message = "서남쪽 경도는 180보다 작거나 같아야 합니다.")
         Float southWestLongitude
 ) {
     @Builder
