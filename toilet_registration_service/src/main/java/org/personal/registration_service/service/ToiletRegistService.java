b17eb0e4bc093cf0b5450f923fc12ef56796b81c
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-12 09:51:00 +0900

    Refactor(regist): 변수명 수정
    변수명 Longitude -> longitude 변경

--- toilet_registration_service/src/main/java/org/personal/registration_service/service/ToiletRegistService.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/service/ToiletRegistService.java
@@ -3,5 +3,5 @@ package org.personal.registration_service.service;
 import org.personal.registration_service.response.ToiletRegistResponse;
 
 public interface ToiletRegistService {
-	ToiletRegistResponse addToiletRegist(final Double latitude, final Double Longitude);
+	ToiletRegistResponse addToiletRegist(final Double latitude, final Double longitude);
 }
