934cc1def1dae1c88df68d691d18b09b0a18c8fd
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-13 21:38:03 +0900

    Feat(regist-read): Service 전체 조회 구현
    Service 전체 조회 구현 완료. Entity가 비었을 시 예외처리 적용

--- toilet_registration_service/src/main/java/org/personal/registration_service/service/impl/ToiletRegistApproveServiceImpl.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/service/impl/ToiletRegistApproveServiceImpl.java
@@ -3,17 +3,18 @@ package org.personal.registration_service.service.impl;
 import static org.personal.registration_service.common.Constants.*;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import org.personal.registration_service.common.ToiletRegistErrorResult;
 import org.personal.registration_service.domain.ToiletRegist;
 import org.personal.registration_service.exception.ToiletRegistException;
-import org.personal.registration_service.repository.QueryDSLRepository;
 import org.personal.registration_service.repository.ToiletRegistRepository;
 import org.personal.registration_service.request.ToiletRegistApproveRequest;
 import org.personal.registration_service.response.ToiletRegistApproveResponse;
 import org.personal.registration_service.response.ToiletRegistResponse;
 import org.personal.registration_service.service.ToiletRegistApproveService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -44,8 +45,22 @@ public class ToiletRegistApproveServiceImpl implements ToiletRegistApproveServic
 
 	@Override
 	public ToiletRegistResponse getToiletRegist(long toiletRegistId) {
-		ToiletRegist toiletRegist = toiletRegistRepository.findBytoiletRegistId(toiletRegistId)
+		ToiletRegist toiletRegist = toiletRegistRepository.findByToiletRegistId(toiletRegistId)
 			.orElseThrow(() -> new ToiletRegistException(ToiletRegistErrorResult.ENTITY_NOT_FOUND));
 		return ToiletRegistResponse.of(toiletRegist);
+	}
+
+	@Override
+	public Page<ToiletRegistResponse> listToiletRegist(int pageNum) {
+		Pageable pageable = PageRequest.of(pageNum, PAGE_SIZE);
+
+		Page<ToiletRegist> toiletRegists = toiletRegistRepository.findAllByPageable(pageable);
+
+		// 조회된 결과가 비어 있는지 확인
+		if (toiletRegists.isEmpty()) {
+			throw new ToiletRegistException(ToiletRegistErrorResult.ENTITY_NOT_FOUND);
+		}
+
+		return toiletRegists.map(ToiletRegistResponse::of);
 	}
 }
