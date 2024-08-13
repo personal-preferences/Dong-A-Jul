f47e57c30b96790fd9e162f6a6e0e3297300d3e5
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-13 19:03:49 +0900

    Feat(regist-read): 특정 화장실등록 조회 Service
    toiletRegistId 값에 맞는 데이터가 없으면 ENTITY_NOT_FOUND 예외처리.

--- toilet_registration_service/src/main/java/org/personal/registration_service/service/ToiletRegistApproveService.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/service/ToiletRegistApproveService.java
@@ -2,7 +2,10 @@ package org.personal.registration_service.service;
 
 import org.personal.registration_service.request.ToiletRegistApproveRequest;
 import org.personal.registration_service.response.ToiletRegistApproveResponse;
+import org.personal.registration_service.response.ToiletRegistResponse;
 
 public interface ToiletRegistApproveService {
 	ToiletRegistApproveResponse updateToiletRegistApprove(ToiletRegistApproveRequest request);
+
+	ToiletRegistResponse getToiletRegist(long toiletRegistId);
 }
--- toilet_registration_service/src/main/java/org/personal/registration_service/service/impl/ToiletRegistApproveServiceImpl.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/service/impl/ToiletRegistApproveServiceImpl.java
@@ -3,13 +3,16 @@ package org.personal.registration_service.service.impl;
 import static org.personal.registration_service.common.Constants.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.personal.registration_service.common.ToiletRegistErrorResult;
 import org.personal.registration_service.domain.ToiletRegist;
 import org.personal.registration_service.exception.ToiletRegistException;
+import org.personal.registration_service.repository.QueryDSLRepository;
 import org.personal.registration_service.repository.ToiletRegistRepository;
 import org.personal.registration_service.request.ToiletRegistApproveRequest;
 import org.personal.registration_service.response.ToiletRegistApproveResponse;
+import org.personal.registration_service.response.ToiletRegistResponse;
 import org.personal.registration_service.service.ToiletRegistApproveService;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +27,7 @@ public class ToiletRegistApproveServiceImpl implements ToiletRegistApproveServic
 	@Override
 	public ToiletRegistApproveResponse updateToiletRegistApprove(ToiletRegistApproveRequest request) {
 		final ToiletRegist toiletRegist = toiletRegistRepository.findById(request.toiletRegistId())
-		.orElseThrow(() -> new ToiletRegistException(ToiletRegistErrorResult.ENTITY_NOT_FOUND));
+			.orElseThrow(() -> new ToiletRegistException(ToiletRegistErrorResult.ENTITY_NOT_FOUND));
 
 		// 이미 등록 처리된 화장실인지 확인
 		if (toiletRegist.getToiletRegistConfirmedDate() != null) {
@@ -37,5 +40,12 @@ public class ToiletRegistApproveServiceImpl implements ToiletRegistApproveServic
 		String status = request.isApproved() ? APPROVE : REJECT;
 
 		return new ToiletRegistApproveResponse(status);
+	}
+
+	@Override
+	public ToiletRegistResponse getToiletRegist(long toiletRegistId) {
+		ToiletRegist toiletRegist = toiletRegistRepository.findBytoiletRegistId(toiletRegistId)
+			.orElseThrow(() -> new ToiletRegistException(ToiletRegistErrorResult.ENTITY_NOT_FOUND));
+		return ToiletRegistResponse.of(toiletRegist);
 	}
 }
