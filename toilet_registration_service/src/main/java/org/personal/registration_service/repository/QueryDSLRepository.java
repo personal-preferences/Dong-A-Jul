45fe42b202259762a082d3112f973389707c79d6
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-13 21:34:01 +0900

    Feat(regist-read): Repository 전체 조회
    QueryDSL 사용하여 전체 조회 구현. Pageable 적용으로 10개씩 조회 페이징 처리 적용

--- toilet_registration_service/src/main/java/org/personal/registration_service/repository/QueryDSLRepository.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/repository/QueryDSLRepository.java
@@ -3,8 +3,13 @@ package org.personal.registration_service.repository;
 import java.util.Optional;
 
 import org.personal.registration_service.domain.ToiletRegist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface QueryDSLRepository {
 
-	Optional<ToiletRegist> findBytoiletRegistId(long toiletRegistId);
+	Optional<ToiletRegist> findByToiletRegistId(long toiletRegistId);
+
+	Page<ToiletRegist> findAllByPageable(Pageable pageable);
 }
+
--- toilet_registration_service/src/main/java/org/personal/registration_service/repository/impl/QueryDSLRepositoryImpl.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/repository/impl/QueryDSLRepositoryImpl.java
@@ -2,11 +2,15 @@ package org.personal.registration_service.repository.impl;
 
 import static org.personal.registration_service.domain.QToiletRegist.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.personal.registration_service.domain.ToiletRegist;
 import org.personal.registration_service.repository.QueryDSLRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -22,12 +26,27 @@ public class QueryDSLRepositoryImpl implements QueryDSLRepository {
 	}
 
 	@Override
-	public Optional<ToiletRegist> findBytoiletRegistId(long toiletRegistId) {
+	public Optional<ToiletRegist> findByToiletRegistId(long toiletRegistId) {
 		ToiletRegist result = queryFactory
 			.selectFrom(toiletRegist)
 			.where(toiletRegist.toiletRegistId.eq(toiletRegistId))
 			.fetchOne();
 
 		return Optional.ofNullable(result);
+	}
+
+
+	public Page<ToiletRegist> findAllByPageable(Pageable pageable) {
+		List<ToiletRegist> results = queryFactory
+			.selectFrom(toiletRegist)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		long total = queryFactory
+			.selectFrom(toiletRegist)
+			.fetchCount();
+
+		return new PageImpl<>(results, pageable, total);
 	}
 }
