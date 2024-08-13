ccfc299967ea702b64886fb95504ff988ab26509
ms1011 <wkdalstjr94@gmail.com>
2024-08-13 16:44:54 +0900

    feat(location): 보이는 영역만 화장실 위치 정보 가져와서 마커 표시하기

--- front/src/components/NaverMaps.vue
+++ front/src/components/NaverMaps.vue
@@ -4,42 +4,9 @@
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import axios from 'axios'
 
 const mapRef = ref(null);
-const MARKER_SPRITE_X_OFFSET = 29;
-const MARKER_SPRITE_Y_OFFSET = 50;
-const MARKER_SPRITE_POSITION = {
-  "A0": [0, 0],
-  "B0": [MARKER_SPRITE_X_OFFSET, 0],
-  "C0": [MARKER_SPRITE_X_OFFSET*2, 0],
-  "D0": [MARKER_SPRITE_X_OFFSET*3, 0],
-  "E0": [MARKER_SPRITE_X_OFFSET*4, 0],
-  "F0": [MARKER_SPRITE_X_OFFSET*5, 0],
-  "G0": [MARKER_SPRITE_X_OFFSET*6, 0],
-  "H0": [MARKER_SPRITE_X_OFFSET*7, 0],
-  "I0": [MARKER_SPRITE_X_OFFSET*8, 0],
-
-  "A1": [0, MARKER_SPRITE_Y_OFFSET],
-  "B1": [MARKER_SPRITE_X_OFFSET, MARKER_SPRITE_Y_OFFSET],
-  "C1": [MARKER_SPRITE_X_OFFSET*2, MARKER_SPRITE_Y_OFFSET],
-  "D1": [MARKER_SPRITE_X_OFFSET*3, MARKER_SPRITE_Y_OFFSET],
-  "E1": [MARKER_SPRITE_X_OFFSET*4, MARKER_SPRITE_Y_OFFSET],
-  "F1": [MARKER_SPRITE_X_OFFSET*5, MARKER_SPRITE_Y_OFFSET],
-  "G1": [MARKER_SPRITE_X_OFFSET*6, MARKER_SPRITE_Y_OFFSET],
-  "H1": [MARKER_SPRITE_X_OFFSET*7, MARKER_SPRITE_Y_OFFSET],
-  "I1": [MARKER_SPRITE_X_OFFSET*8, MARKER_SPRITE_Y_OFFSET],
-
-  "A2": [0, MARKER_SPRITE_Y_OFFSET*2],
-  "B2": [MARKER_SPRITE_X_OFFSET, MARKER_SPRITE_Y_OFFSET*2],
-  "C2": [MARKER_SPRITE_X_OFFSET*2, MARKER_SPRITE_Y_OFFSET*2],
-  "D2": [MARKER_SPRITE_X_OFFSET*3, MARKER_SPRITE_Y_OFFSET*2],
-  "E2": [MARKER_SPRITE_X_OFFSET*4, MARKER_SPRITE_Y_OFFSET*2],
-  "F2": [MARKER_SPRITE_X_OFFSET*5, MARKER_SPRITE_Y_OFFSET*2],
-  "G2": [MARKER_SPRITE_X_OFFSET*6, MARKER_SPRITE_Y_OFFSET*2],
-  "H2": [MARKER_SPRITE_X_OFFSET*7, MARKER_SPRITE_Y_OFFSET*2],
-  "I2": [MARKER_SPRITE_X_OFFSET*8, MARKER_SPRITE_Y_OFFSET*2]
-};
-
 
 onMounted(async () => {
   try {
@@ -55,32 +22,44 @@ onMounted(async () => {
       const bounds = map.getBounds();
       const southWest = bounds.getSW();
       const northEast = bounds.getNE();
-      const lngSpan = northEast.lng() - southWest.lng();
-      const latSpan = northEast.lat() - southWest.lat();
+
+      let locationMarkers = []
+
+      await axios.get('http://localhost:8080/locations', {
+        params: {
+          northEastLatitude: northEast.lat(),
+          northEastLongitude: northEast.lng(),
+          southWestLatitude: southWest.lat(),
+          southWestLongitude: southWest.lng()
+        }
+      })
+        .then(response => {
+          locationMarkers = response.data;
+        })
+        .catch(axiosError => {
+          console.log('마커 요청 오류 발생 : ', axiosError)
+        });
 
       const markers = [];
 
-      for (const key in MARKER_SPRITE_POSITION) {
-        const position = new window.naver.maps.LatLng(
-            southWest.lat() + latSpan * Math.random(),
-            southWest.lng() + lngSpan * Math.random()
-        );
+      // 새로운 마커 추가
+      locationMarkers.forEach(location => {
+        const position = new naver.maps.LatLng(location.latitude, location.longitude)
 
-        const marker = new window.naver.maps.Marker({
+        const marker = new naver.maps.Marker({
           map: map,
           position: position,
-          title: key,
+          title: location.name,   // 마커에 마우스를 올렸을 때 이름 표시
           icon: {
             content: '<p>🚽</p>',
-            size: new window.naver.maps.Size(24, 37),
-            anchor: new window.naver.maps.Point(12, 37),
-            origin: new window.naver.maps.Point(MARKER_SPRITE_POSITION[key][0], MARKER_SPRITE_POSITION[key][1])
+            size: new naver.maps.Size(24, 37),
+            anchor: new naver.maps.Point(12, 37)
           },
           zIndex: 100
-        });
+        })
 
         markers.push(marker);
-      }
+      });
 
       window.naver.maps.Event.addListener(map, 'idle', () => {
         updateMarkers(map, markers);
