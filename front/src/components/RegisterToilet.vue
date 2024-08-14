db463b501cda1525d64828d34e9e83381740765f
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-14 16:32:56 +0900

    Feat(front-regist): 화장실 등록 신청
    화장실 등록 신청 기능 구현 완료.

--- front/src/components/RegisterToilet.vue
+++ front/src/components/RegisterToilet.vue
@@ -24,17 +24,18 @@
         <label for="toiletRegistLongitude">Longitude:</label>
         <input type="number" step="any" id="toiletRegistLongitude" v-model="form.toiletRegistLongitude" required />
       </div>
+      <!-- 선택 입력 필드들 -->
       <div>
         <label for="toiletRegistManagementAgency">Management Agency:</label>
-        <input id="toiletRegistManagementAgency" v-model="form.toiletRegistManagementAgency" required />
+        <input id="toiletRegistManagementAgency" v-model="form.toiletRegistManagementAgency" />
       </div>
       <div>
         <label for="toiletRegistPhoneNumber">Phone Number:</label>
-        <input id="toiletRegistPhoneNumber" v-model="form.toiletRegistPhoneNumber" required />
+        <input id="toiletRegistPhoneNumber" v-model="form.toiletRegistPhoneNumber" />
       </div>
       <div>
         <label for="toiletRegistOpeningHours">Opening Hours:</label>
-        <input id="toiletRegistOpeningHours" v-model="form.toiletRegistOpeningHours" required />
+        <input id="toiletRegistOpeningHours" v-model="form.toiletRegistOpeningHours" />
       </div>
       <div>
         <label for="toiletRegistOpeningHoursDetails">Opening Hours Details:</label>
@@ -42,15 +43,15 @@
       </div>
       <div>
         <label for="toiletRegistInstallationYearMonth">Installation Year/Month:</label>
-        <input id="toiletRegistInstallationYearMonth" v-model="form.toiletRegistInstallationYearMonth" required />
+        <input id="toiletRegistInstallationYearMonth" v-model="form.toiletRegistInstallationYearMonth" />
       </div>
       <div>
         <label for="toiletRegistOwnershipType">Ownership Type:</label>
-        <input id="toiletRegistOwnershipType" v-model="form.toiletRegistOwnershipType" required />
+        <input id="toiletRegistOwnershipType" v-model="form.toiletRegistOwnershipType" />
       </div>
       <div>
         <label for="toiletRegistWasteDisposalMethod">Waste Disposal Method:</label>
-        <input id="toiletRegistWasteDisposalMethod" v-model="form.toiletRegistWasteDisposalMethod" required />
+        <input id="toiletRegistWasteDisposalMethod" v-model="form.toiletRegistWasteDisposalMethod" />
       </div>
       <div>
         <label for="toiletInfoSafetyFacilityInstallationIsRequired">Safety Facility Installation Required:</label>
@@ -78,39 +79,39 @@
       </div>
       <div>
         <label for="toiletRegistMaleToiletsNumber">Male Toilets Number:</label>
-        <input type="number" id="toiletRegistMaleToiletsNumber" v-model="form.toiletRegistMaleToiletsNumber" required />
+        <input type="number" id="toiletRegistMaleToiletsNumber" v-model="form.toiletRegistMaleToiletsNumber" />
       </div>
       <div>
         <label for="toiletRegistMaleUrinalsNumber">Male Urinals Number:</label>
-        <input type="number" id="toiletRegistMaleUrinalsNumber" v-model="form.toiletRegistMaleUrinalsNumber" required />
+        <input type="number" id="toiletRegistMaleUrinalsNumber" v-model="form.toiletRegistMaleUrinalsNumber" />
       </div>
       <div>
         <label for="toiletRegistMaleDisabledToiletsNumber">Male Disabled Toilets Number:</label>
-        <input type="number" id="toiletRegistMaleDisabledToiletsNumber" v-model="form.toiletRegistMaleDisabledToiletsNumber" required />
+        <input type="number" id="toiletRegistMaleDisabledToiletsNumber" v-model="form.toiletRegistMaleDisabledToiletsNumber" />
       </div>
       <div>
         <label for="toiletRegistMaleDisabledUrinalsNumber">Male Disabled Urinals Number:</label>
-        <input type="number" id="toiletRegistMaleDisabledUrinalsNumber" v-model="form.toiletRegistMaleDisabledUrinalsNumber" required />
+        <input type="number" id="toiletRegistMaleDisabledUrinalsNumber" v-model="form.toiletRegistMaleDisabledUrinalsNumber" />
       </div>
       <div>
         <label for="toiletRegistMaleChildToiletsNumber">Male Child Toilets Number:</label>
-        <input type="number" id="toiletRegistMaleChildToiletsNumber" v-model="form.toiletRegistMaleChildToiletsNumber" required />
+        <input type="number" id="toiletRegistMaleChildToiletsNumber" v-model="form.toiletRegistMaleChildToiletsNumber" />
       </div>
       <div>
         <label for="toiletRegistMaleChildUrinalsNumber">Male Child Urinals Number:</label>
-        <input type="number" id="toiletRegistMaleChildUrinalsNumber" v-model="form.toiletRegistMaleChildUrinalsNumber" required />
+        <input type="number" id="toiletRegistMaleChildUrinalsNumber" v-model="form.toiletRegistMaleChildUrinalsNumber" />
       </div>
       <div>
         <label for="toiletRegistFemaleToiletsNumber">Female Toilets Number:</label>
-        <input type="number" id="toiletRegistFemaleToiletsNumber" v-model="form.toiletRegistFemaleToiletsNumber" required />
+        <input type="number" id="toiletRegistFemaleToiletsNumber" v-model="form.toiletRegistFemaleToiletsNumber" />
       </div>
       <div>
         <label for="toiletRegistFemaleDisabledToiletsNumber">Female Disabled Toilets Number:</label>
-        <input type="number" id="toiletRegistFemaleDisabledToiletsNumber" v-model="form.toiletRegistFemaleDisabledToiletsNumber" required />
+        <input type="number" id="toiletRegistFemaleDisabledToiletsNumber" v-model="form.toiletRegistFemaleDisabledToiletsNumber" />
       </div>
       <div>
         <label for="toiletRegistFemaleChildToiletsNumber">Female Child Toilets Number:</label>
-        <input type="number" id="toiletRegistFemaleChildToiletsNumber" v-model="form.toiletRegistFemaleChildToiletsNumber" required />
+        <input type="number" id="toiletRegistFemaleChildToiletsNumber" v-model="form.toiletRegistFemaleChildToiletsNumber" />
       </div>
       <div>
         <label for="userEmail">User Email:</label>
@@ -147,15 +148,15 @@ export default {
         toiletRegistEntranceCctvIsInstalled: false,
         toiletRegistDiaperChangingTableIsAvailable: false,
         toiletRegistDiaperChangingTableLocation: '',
-        toiletRegistMaleToiletsNumber: 0,
-        toiletRegistMaleUrinalsNumber: 0,
-        toiletRegistMaleDisabledToiletsNumber: 0,
-        toiletRegistMaleDisabledUrinalsNumber: 0,
-        toiletRegistMaleChildToiletsNumber: 0,
-        toiletRegistMaleChildUrinalsNumber: 0,
-        toiletRegistFemaleToiletsNumber: 0,
-        toiletRegistFemaleDisabledToiletsNumber: 0,
-        toiletRegistFemaleChildToiletsNumber: 0,
+        toiletRegistMaleToiletsNumber: null,
+        toiletRegistMaleUrinalsNumber: null,
+        toiletRegistMaleDisabledToiletsNumber: null,
+        toiletRegistMaleDisabledUrinalsNumber: null,
+        toiletRegistMaleChildToiletsNumber: null,
+        toiletRegistMaleChildUrinalsNumber: null,
+        toiletRegistFemaleToiletsNumber: null,
+        toiletRegistFemaleDisabledToiletsNumber: null,
+        toiletRegistFemaleChildToiletsNumber: null,
         userEmail: ''
       },
     };
@@ -164,7 +165,16 @@ export default {
     async submitForm() {
       try {
         console.log('Submitting data:', this.form);  // 데이터 확인
-        const response = await axios.post('http://localhost:8765/toiletregists', this.form);
+        const response = await axios.post(
+          'http://localhost:8765/toiletregists',
+          JSON.stringify(this.form),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'X-USER-ID': 'user123',
+            },
+          }
+        );
         console.log('Registration successful:', response.data);
       } catch (error) {
         console.error('Error submitting form:', error);
