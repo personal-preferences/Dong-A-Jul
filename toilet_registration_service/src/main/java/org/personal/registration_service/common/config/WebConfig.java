f2ac2f6c7078c6b00827cec93b7087584a8f03fb
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-14 16:39:26 +0900

    Refactor(regist-read): CORS 수정
    Patch 추가

--- toilet_registration_service/src/main/java/org/personal/registration_service/common/config/WebConfig.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:5173") // Vue.js 개발 서버 도메인
-			.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "Patch") // 허용할 HTTP 메소드
 			.allowedHeaders("*") // 허용할 헤더
 			.allowCredentials(true) // 자격 증명 허용 (쿠키 등)
 			.maxAge(3600); // 프리플라이트 요청 캐시 시간 (초)
