89852191511132889ae3a01da70aadef854ca7b8
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-14 14:10:13 +0900

    Feat(front-regist): 라우팅 설정
    등록, 조회 라우팅 설정

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,22 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import ToiletRegistList from '@/components/ToiletRegistList.vue'
+import ToiletDetails from '@/components/ToiletDetails.vue';
+import RegisterToilet from '@/components/RegisterToilet.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'ToiletRegistList',
+      component: ToiletRegistList
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    { path: '/details/:id',
+      name: 'ToiletDetails',
+      component: ToiletDetails,
+      props: true
+    },
+    { path: '/register',
+      name: 'RegisterToilet',
+      component: RegisterToilet
+    },
+
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
