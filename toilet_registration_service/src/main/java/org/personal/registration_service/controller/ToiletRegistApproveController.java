4f3fef47a4ae2019ad64ad9fc13b4130ef003c6c
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-08-14 15:53:40 +0900

    Refactor(regist-read): Params
    PathVariable 수정

--- toilet_registration_service/src/main/java/org/personal/registration_service/controller/ToiletRegistApproveController.java
+++ toilet_registration_service/src/main/java/org/personal/registration_service/controller/ToiletRegistApproveController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,8 +51,8 @@ public class ToiletRegistApproveController {
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
-	@GetMapping
-	public ResponseEntity<ToiletRegistResponse> getToiletRegist(@RequestParam long toiletRegistId){
+	@GetMapping("{toiletRegistId}")
+	public ResponseEntity<ToiletRegistResponse> getToiletRegist(@PathVariable long toiletRegistId){
 		ToiletRegistResponse response = toiletRegistApproveService.getToiletRegist(toiletRegistId);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
