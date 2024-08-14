0f2ce6420780f3192304425b1c7f9e7da42a8c4e
daersh <daersh@naver.com>
2024-08-14 10:08:10 +0900

    feat(user): header에 access 토큰 추가
    요청을 보낼 때 헤더에 access토큰을 담도록 했습니다.

--- user_service_FE/user/src/assets/axios.js
+++ user_service_FE/user/src/assets/axios.js
@@ -11,9 +11,9 @@ const instance = axios.create({
 
 // 요청 인터셉터: access token을 헤더에 추가
 instance.interceptors.request.use(config => {
-  const token = localStorage.getItem('accessToken');
+  const token = localStorage.getItem('access');
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.access = token;
   }
   return config;
 }, error => {
