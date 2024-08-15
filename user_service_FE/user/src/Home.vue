6a51797f9d666f890b7788bf2549fe83ec15d3bc
daersh <daersh@naver.com>
2024-08-15 23:43:16 +0900

    fix(user): 사용자 토큰 없을 때에 대한 로직 추가
    홈 화면에서 회원 정보를 조회하려 할 때 토큰이 없는 경우에 대한 코드입니다.

--- user_service_FE/user/src/Home.vue
+++ user_service_FE/user/src/Home.vue
@@ -3,39 +3,61 @@
     <h1>Home Page</h1>
     <p>이곳은 홈 페이지입니다.</p>
   </div>
-  
-  
+
+  <div>
     <h2>회원 정보</h2>
-    <p>이메일: {{ userInfo.userEmail }}</p>
-    <p>이메일: {{ userInfo.userNickname }}</p>
-    <p>이메일: {{ userInfo.userRole }}</p>
+    <!-- userData 값이 있을 경우에만 출력 -->
+    <p>이메일: {{ userData.userEmail || '로그인 필요' }}</p>
+    <p>닉네임: {{ userData.userNickname || '로그인 필요' }}</p>
+    <p>권한: {{ userData.userRole || '로그인 필요' }}</p>
+  </div>
 
-    <div>
-        <input type="button" @click="getUserInfo" value="asdf"/> 
-    </div>
-  </template>
-  
+  <div>
+    <input type="button" @click="getUserInfo" value="정보 가져오기" /> 
+  </div>
+</template>
+
 <script setup>
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  import axios from '@/assets/axios';
-  import { useUserStore } from '@/stores/user';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from '@/assets/axios';
+import { useUserStore } from '@/stores/user';
 
-  const { userInfo } = useUserStore();
-  console.log(userInfo.value.userEmail);
+const userData = ref({
+  userEmail: '',
+  userNickname: '',
+  userRole: ''
+});
 
-  const getUserInfo = async ()=>{
+const { userInfo } = useUserStore();
+
+// userInfo가 변경될 때마다 userData를 업데이트
+watch(userInfo, (newUserInfo) => {
+  if (newUserInfo) {
+    userData.value = newUserInfo;
+  } else {
+    userData.value = {
+      userEmail: '',
+      userNickname: '',
+      userRole: ''
+    };
+  }
+});
+
+// 현재 로그인 정보가 있는지 확인
+if (userInfo.value) {
+  userData.value = userInfo.value;
+}
+
+const getUserInfo = async () => {
   try {
     const response = await axios.get('/users/info');
     console.log('User Info:', response.data);
   } catch (error) {
     console.error('User Info Fetch Error:', error);
+    router.push('/login');
   }
 };
 
-
-
 const router = useRouter();
-
-
 </script>
