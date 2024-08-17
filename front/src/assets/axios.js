2f8c5696f0d6ce3f6dcee7a4d4ae9e242723e081
daersh <daersh@naver.com>
2024-08-17 15:58:52 +0900

    feat(user): 유저 서비스 요청 axios 추가
    유저 서비스에 요청을 보내기 위한 axios 경로, 토큰 관리하기
    위한 스크립트가 있습니다.

---
+++ front/src/assets/axios.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useUserStore } from '@/stores/user';
+
+// Axios 인스턴스 생성
+const instance = axios.create({
+  baseURL: 'http://localhost:8765',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true  // withCredentials 추가
+});
+
+// 요청 인터셉터: access token을 헤더에 추가
+instance.interceptors.request.use(config => {
+  const token = localStorage.getItem('access');
+  if (token) {
+    config.headers.access = token;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+// 응답 인터셉터: 406 응답 시 새 access token 처리
+instance.interceptors.response.use(response => {
+  return response;
+}, async error => {
+  const originalRequest = error.config;
+
+  // 406 상태코드 처리 (새로운 access token 수신 시)
+  if (error.response.status === 406 && error.response.headers['access']) {
+    const newAccessToken = error.response.headers['access'];
+    console.log(`new Access token: ${newAccessToken}`);
+    // 새로운 access token 저장
+    localStorage.setItem('access', newAccessToken);
+
+    // Authorization 헤더 갱신 및 요청 재시도
+    originalRequest.headers.Authorization = newAccessToken;
+    return instance(originalRequest);
+  }
+
+
+  return Promise.reject(error);
+});
+
+export default instance;
