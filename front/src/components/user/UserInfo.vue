ebd3fc7f33aa6befaba165183ad25ab38fadf68b
daersh <daersh@naver.com>
2024-08-17 15:56:51 +0900

    feat(user): 회원 정보 페이지 구현
    회원의 정보를 조회하는 페이지로 토큰을 통한 회원정보 조회,
    버튼을 통해 서버에 요청하여 회원 정보를 조회하는 두가지 방식이 있습니다.

---
+++ front/src/components/user/UserInfo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h2>회원 정보</h2>
+    <!-- userData 값이 있을 경우에만 출력 -->
+    <p>이메일: {{ userData.userEmail || '로그인 필요' }}</p>
+    <p>닉네임: {{ userData.userNickname || '로그인 필요' }}</p>
+    <p>권한: {{ userData.userRole || '로그인 필요' }}</p>
+  </div>
+
+  <div>
+    <input type="button" @click="getUserInfo" value="정보 가져오기" /> 
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from '@/assets/axios';
+import { useUserStore } from '@/stores/user';
+
+const userData = ref({
+  userEmail: '',
+  userNickname: '',
+  userRole: ''
+});
+
+const { userInfo } = useUserStore();
+
+// userInfo가 변경될 때마다 userData를 업데이트
+watch(userInfo, (newUserInfo) => {
+  if (newUserInfo) {
+    userData.value = newUserInfo;
+  } else {
+    userData.value = {
+      userEmail: '',
+      userNickname: '',
+      userRole: ''
+    };
+  }
+});
+
+// 현재 로그인 정보가 있는지 확인
+if (userInfo.value) {
+  userData.value = userInfo.value;
+}
+
+const getUserInfo = async () => {
+  try {
+    const response = await axios.get('/users/info');
+    console.log('User Info:', response.data);
+  } catch (error) {
+    console.error('User Info Fetch Error:', error);
+    router.push('/login');
+  }
+};
+
+const router = useRouter();
+</script>
