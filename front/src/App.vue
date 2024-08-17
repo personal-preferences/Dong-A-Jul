5a388c03daee492235e40bb435544b76146d2a3a
daersh <daersh@naver.com>
2024-08-17 16:00:14 +0900

    fix(user): App.vue 라우터 추가
    App.vue에 라우터들을 추가했습니다.

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
-import { RouterView } from 'vue-router'
+// import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div>
+   <nav>
+      <router-link to="/">Home</router-link>
+      &nbsp;
+      <router-link to="/login">Login</router-link>
+      &nbsp;
+      <router-link to="/regist">Regist</router-link>
+      &nbsp;
+      <router-link to="/userInfo">userInfo</router-link>
+    </nav>
   <RouterView />
+  </div>
 </template>
 
 <style scoped>
