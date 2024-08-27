1cb18e2907708e39161693f2d0a04e3b9a6d947f
ms1011 <wkdalstjr94@gmail.com>
2024-08-27 14:35:57 +0900

    feat(location): postgreSQL db 연결, h2db yml 분리
    테스트 코드에서 사용하는 profile 설정 추가

--- location_service/src/test/java/org/personal/locations_service/controller/LocationControllerTest.java
+++ location_service/src/test/java/org/personal/locations_service/controller/LocationControllerTest.java
@@ -13,6 +13,7 @@ import org.personal.locations_service.response.LocationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class LocationControllerTest {
 
     @Autowired
--- location_service/src/test/java/org/personal/locations_service/service/LocationServiceTest.java
+++ location_service/src/test/java/org/personal/locations_service/service/LocationServiceTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LocationServiceTest {
 
     @Autowired
