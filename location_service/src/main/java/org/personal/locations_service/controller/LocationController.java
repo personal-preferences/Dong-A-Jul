7faacbaac1086c29a04c72d01f36508c7c25649e
ms1011 <wkdalstjr94@gmail.com>
2024-08-29 15:59:09 +0900

    remove(location): 카프카 통신 완료로 인한 코드 삭제

--- location_service/src/main/java/org/personal/locations_service/controller/LocationController.java
+++ location_service/src/main/java/org/personal/locations_service/controller/LocationController.java
@@ -3,14 +3,11 @@ package org.personal.locations_service.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.personal.locations_service.messaging.LocationEventProducer;
 import org.personal.locations_service.request.LocationCreate;
 import org.personal.locations_service.request.LocationEdit;
 import org.personal.locations_service.request.LocationMarker;
 import org.personal.locations_service.response.LocationResponse;
 import org.personal.locations_service.service.LocationService;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +21,6 @@ import java.util.List;
 public class LocationController {
 
     private final LocationService locationService;
-    private final LocationEventProducer locationEventProducer;
 
     @PostMapping
     public void add(@RequestBody @Valid LocationCreate locationCreate) {
@@ -58,20 +54,4 @@ public class LocationController {
     @DeleteMapping("/{locationId}")
     public void delete(@PathVariable Long locationId) { locationService.delete(locationId); }
 
-    @GetMapping("/kafka")
-    public String messageProduce(){
-        LocationCreate request = LocationCreate
-                .builder()
-                .name("홍대 화장실")
-                .roadAddress("서울 마포구 서교동 1길")
-                .jibunAddress("서울 마포구 동교동 150-1")
-                .latitude(10.23f)
-                .longitude(32.99f)
-                .build();
-
-        locationEventProducer.syncSend("toilet-location-deleted-sync", "삭제됨");
-        locationEventProducer.asyncSend("toilet-location-deleted", request);
-
-        return "Kafka service started";
-    }
 }
