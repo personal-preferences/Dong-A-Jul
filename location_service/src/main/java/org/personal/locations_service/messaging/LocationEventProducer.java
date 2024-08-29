b728bf3df675197e41b8b12f3d54b2f8b3047918
ms1011 <wkdalstjr94@gmail.com>
2024-08-29 15:34:54 +0900

    feat(location): 메시지 발행 예외처리 추가, 헤더 정보 추가

--- location_service/src/main/java/org/personal/locations_service/messaging/LocationEventProducer.java
+++ location_service/src/main/java/org/personal/locations_service/messaging/LocationEventProducer.java
@@ -1,25 +1,35 @@
 package org.personal.locations_service.messaging;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.KafkaException;
 import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class LocationEventProducer {
 
     private final StreamBridge streamBridge;
+    private static final String SERVICE_NAME = "location-service";
 
-    @Async
-    public void asyncSend(String topic, Object message) {
-        streamBridge.send(topic, message);
+    public void send(String topic, Object payload) {
+        Message<Object> message = createMessage(payload);
+
+        try {
+            streamBridge.send(topic, message);
+        } catch (KafkaException e){
+            log.error("[{}] 메시지 발행 실패하였습니다. {}", topic, e.getMessage());
+        }
     }
 
-    public void syncSend(String topic, Object message) {
-        boolean result = streamBridge.send(topic, message);
-        if(!result) {
-            throw new RuntimeException("Failed to send message to topic: " + topic + ", message: " + message);
-        }
+    private Message<Object> createMessage(Object payload) {
+        return MessageBuilder
+                .withPayload(payload)
+                .setHeader("source", SERVICE_NAME)
+                .build();
     }
 }
